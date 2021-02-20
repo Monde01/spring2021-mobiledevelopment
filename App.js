ae0bf927de220570a9538aa12e3f73e9dfa99cad
Monde01 <77816037+Monde01@users.noreply.github.com>
2021-02-19 19:56:03 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+
+import Example from './components/Example/index.';
+
+function App() {
+  return (
+    <Example/>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-// Import Article
-import Article from './Article.js';
+import React from 'react';
 
-// Pass the values to the constructor method of Article
-const a = new Article (
-  {
-    title: "This is the title!",
-    author: "Me",
-    text: "This is an example article!"
-  }
-);
-
-// Shows its HTML on the console
-console.log(a.render() );
-
-class ArticleTitle {
-
-  constructor(title) {
-    this.props = {};
-    this.props.title = title;
-  }
-
-  render() {
-    return `<h1>${this.props.title}</h1>`;
-  }
-
+class Example extends React.Component {
+    render() {
+        return <p>My name is Maria Onde</p>;
+    }
 }
+
+export default Example;
